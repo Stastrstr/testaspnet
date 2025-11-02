33fa3d2cfd41e8417d31afc15f032ed625fcb97a
troop <troop3r3@gmail.com>
2025-11-02 21:15:05 +0400

    add nice weather

--- PracticeOneSample/Program.cs
+++ PracticeOneSample/Program.cs
@@ -22,7 +22,7 @@ else
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Nice"
 };
 
 app.MapGet("/weatherforecast", () =>
